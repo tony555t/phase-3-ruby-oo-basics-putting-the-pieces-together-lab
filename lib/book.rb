74ea45b2de45218b058329991928c5776a16be4d
tony555t <tonytullah764@gmail.com>
2023-02-14 04:30:09 +0300

    initialization

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+    require "pry"
+    class Book
+        attr_accessor :author,:page_count,:genre
+        attr_reader :title
 
+        def initialize(title)
+        @title = title
+        end
+        def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        end
+    end
 
+atomic=Book.new("harypoter")
+atomic.author ="poter"
+atomic.genre = "fiction"
+atomic.page_count = 300
+atomic.turn_page
